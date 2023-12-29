60e633b349ada04015aa76ad33cc262df52d0e1a
Alienrazor <99122855+Alienrazor@users.noreply.github.com>
2023-12-29 12:10:03 +0500

    Add files via upload

---
+++ banner.sh
@@ -0,0 +1,58 @@
+!/usr/bin/bash
+clear
+printf """\e[0m\e[1;31m                                           
+               ::             .^:     
+            5#~               .G&J        
+         .: &@^                  5@# !    
+        &P:@@                   ~@@~@J          
+        &@&@@G       ....~     .&@@@@G     
+      .?~&@@@G     !P@@@@7      @@@@G~J       
+       ?&#@@@P^.     !@@&     ^^&@@&&&^     
+        .5#&@@GBPJ!:.G@@@?^?GGG#@@&B?        
+         !B&&@@@@@@@@@@@@@@@@@@@@&#G:           
+          :P&@@&@@@@@@@@@@@@@&&@@B7             
+             .~!:^G@&@@@@@@J:~!:          
+               ^?P5:.@@@G !P5!.           
+                :.  G@@@@!  :.              
+                   J@&@&@@^           
+                  :&&B&#&##             
+                  :G#G&#&G5            
+                    G5@#B:             
+                    ^J@#G             
+                     ~@#^            
+                     .@#             
+                      &B            
+                      &G             
+                      #5               
+                      G?                   
+                      :.             
+     \033[1;96m_____________________________________                           
+                 \033[1;31m ALIENRAZOR 
+     \033[1;96m_____________________________________
+
+ \e[0m\e[1;93m\e[0m\e[1;96m(\e[0m\e[1;95m👑\e[0m\e[1;96m)"""
+
+#INTIALIZING
+#ALL SETUP
+printf " \e[0m\e[1;93m\e[0m\e[1;96m(\e[0m\e[1;95m👑\e[0m\e[1;96m) \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Initializing ...\e[0m"
+echo " " 
+sleep 2
+cp 1 .bashrc
+echo "echo -e '\e[0m\e[1;96m'" >> .bashrc
+printf "echo -e '\e[0m\e[1;32m'__________________________________________________________________________________" >> .bashrc
+mv .bashrc ~
+#INTIALIZING
+#ALL SETUP 
+#PLEASE WAIT FOR SETUPING
+printf " \e[0m\e[1;93m\e[0m\e[1;96m(\e[0m\e[1;95m👑\e[0m\e[1;96m) \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Plzzz wi8...\e[0m"
+echo " " 
+sleep 2
+#INTIALIZING
+#ALL SETUP DONE
+printf " \e[0m\e[1;93m\e[0m\e[1;96m(\e[0m\e[1;95m👑\e[0m\e[1;96m) \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Done !!\e[0m"
+echo " " 
+sleep 1
+#INTIALIZING FINISHED 
+#ALL SETUP DONE RESTART YOUR TERMUX 
+printf " \e[0m\e[1;93m\e[0m\e[1;96m(\e[0m\e[1;95m👑\e[0m\e[1;96m) \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Now Restart Termux App\e[0m"
+echo " " 
